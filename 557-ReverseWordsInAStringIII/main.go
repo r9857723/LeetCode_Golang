c7778e15f866fa1c98760f66793968502a0cb81d
RomanHsieh <r9857723@gmail.com>
2021-12-20 21:49:24 +0800

    557 ok

--- 557-ReverseWordsInAStringIII/main.go
+++ 557-ReverseWordsInAStringIII/main.go
@@ -1,5 +1,25 @@
 package main
 
-func reverseWords(s string) string {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+
+	q := "Let's take LeetCode contest"
+	//q = "s'teL ekat edoCteeL tsetnoc"
+	fmt.Println(reverseWords(q))
+}
+
+func reverseWords(s string) string {
+	rtn := ""
+	for _, g := range strings.Split(s," ") {
+		str := ""
+		for i := len(g) - 1 ; i >= 0 ; i-- {
+			str += string(g[i])
+		}
+			rtn += " " + str
+	}
+	return strings.TrimSpace(rtn)
 }
--- 557-ReverseWordsInAStringIII/main_test.go
+++ 557-ReverseWordsInAStringIII/main_test.go
@@ -3,5 +3,24 @@ package main
 import "testing"
 
 func Test_(t *testing.T) {
+	q1, a1 := test1()
+	if rtn := reverseWords(q1); rtn != a1 {
+		t.Errorf("error with test1 = %v , wnat = %v", rtn, a1)
+	}
+	q2, a2 := test1()
+	if rtn := reverseWords(q2); rtn != a2 {
+		t.Errorf("error with test1 = %v , wnat = %v", rtn, a2)
+	}
+}
 
+func test1() (q, a string) {
+	q = "s'teL ekat edoCteeL tsetnoc"
+	a = "Let's take LeetCode contest"
+	return q, a
+}
+
+func test2()(q, a string) {
+	q = "God Ding"
+	a = "doG gniD"
+	return q, a
 }
